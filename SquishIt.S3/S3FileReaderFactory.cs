19059cb87b309bdc11526875d78b2f1c98b8460a
Alex Ullrich <alex.ullrich@gmail.com>
2012-05-03 10:16:09 -0400

    stubbing out factories

---
+++ SquishIt.S3/S3FileReaderFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SquishIt.Framework.Files;
+
+namespace SquishIt.S3
+{
+    public class S3FileReaderFactory : IFileReaderFactory
+    {
+        public IFileReader GetFileReader(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FileExists(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SquishIt.S3/S3FileWriterFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SquishIt.Framework.Files;
+
+namespace SquishIt.S3
+{
+    public class S3FileWriterFactory : IFileWriterFactory
+    {
+        public IFileWriter GetFileWriter(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
