c4307a1bf1b9a12785a6a0484726f2ffa0d9b9fe
Alex Ullrich <alex.ullrich@gmail.com>
2012-05-12 17:26:21 -0400

    adding App_Start code for webActivator - it should introduce compilation errors to point user to where credentials and bucket name are supplied (start #1)

---
+++ SquishIt.S3/Content/App_Start/SquishItS3.cs.pp
@@ -0,0 +1,25 @@
+﻿﻿[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SquishItS3), "Start")]
+
+namespace $rootnamespace$.App_Start
+{
+	using SquishIt.Framework;
+	using SquishIt.S3;
+
+	public class SquishItS3
+	{
+		public static void Start()
+		{
+			var s3client = new AmazonS3Client(awsAccessKey, awsSecretKey);
+            var renderer = S3Renderer.Create(s3client)
+                .WithBucketName(bucketName)
+                .WithDefaultKeyBuilder(HttpContext.Current.Request.PhysicalApplicationPath,
+                                               HttpContext.Current.Request.ApplicationPath)
+                .WithCannedAcl(S3CannedACL.PublicRead) as IRenderer;
+
+            Bundle.ConfigureDefaults()
+                .UseReleaseRenderer(renderer)
+                .UseDefaultOutputBaseHref("http://s3.amazonaws.com/" + bucketName);
+
+		}
+	}
+}
