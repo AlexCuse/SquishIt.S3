398ddbb7ff429a0c071e4f0cd9cdf87ed58746b3
Alex Ullrich <alex.ullrich@gmail.com>
2016-08-31 16:44:36 -0400

    update nuspec to 0.9.8, include source parameter on push

--- pack-push.bat
+++ pack-push.bat
@@ -1,6 +1,6 @@
-msbuild SquishIt.S3.sln /p:Configuration=Release
+msbuild SquishIt.S3.sln /p:Configuration=Release /p:Platform="Any CPU"
 nuget pack squishit.s3.nuspec
 for %%p in (*.nupkg) do (
-	nuget push %%p
+	nuget push %%p -Source https://www.nuget.org/api/v2/package
 	del %%p
 )
